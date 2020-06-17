5c5cda67fbdd2da71c3512f7afe64d424ab6a82a
divineLush <homeoflucifer@gmail.com>
2020-06-17 14:10:51 +0700

    vue-localstorage setup

--- src/components/AppHome.vue
+++ src/components/AppHome.vue
@@ -1,9 +1,52 @@
-<template>
-    <p>home</p>
+<template lang="pug">
+    div
+        button(@click="addNote") Add note
+        div(v-for="note in notes")
+            p {{ note.title }}
+            div(v-for="todo in note.todos")
+                p {{ todo.name }}
+                p {{ todo.isCompleted }}
 </template>
 
 <script>
 export default {
-    name: 'AppHome'
+    name: 'AppHome',
+
+    data() {
+        return {
+            notes: []
+        }
+    },
+
+    mounted() {
+        // this.$localStorage.remove('notes')
+        const notes = JSON.parse(this.$localStorage.get('notes'))
+        if (notes)
+            this.notes = notes
+    },
+
+    methods: {
+        addNote() {
+            const note = {
+                title: 'title',
+                todos: [
+                    {
+                        name: '1',
+                        isCompleted: false
+                    },
+                    {
+                        name: '2',
+                        isCompleted: true
+                    }
+                ]
+            }
+            this.notes.push(note)
+            this.saveNotes()
+        },
+        saveNotes() {
+            const notes = JSON.stringify(this.notes)
+            this.$localStorage.set('notes', notes)
+        }
+    }
 }
 </script>
