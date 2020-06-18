e29bcbb8bf448cd29e8a817f27d3d48c9cad920b
divineLush <homeoflucifer@gmail.com>
2020-06-18 18:10:41 +0700

    custom checkboxes

--- src/components/AppNoteTodos.vue
+++ src/components/AppNoteTodos.vue
@@ -4,9 +4,13 @@
         div.todo(v-for="(todo, i) in note.todos" :key="i")
             label.label(:for="`${inputID}${i}`") Todo Name
             input(v-model="todo.name" :id="`${inputID}${i}`")
-            template(v-if="hasDeleteProp")
-                input.todo__checkbox(type="checkbox" v-model="todo.isCompleted")
-                button.btn(@click="onDelete(todo)") Delete
+            div.todo__control(v-if="hasDeleteProp" style="display: flex")
+                div
+                    label.todo__checkbox-container
+                        input.todo__checkbox-container__checkbox(type="checkbox" v-model="todo.isCompleted")
+                        span.todo__checkbox-container__checkmark
+                div
+                    button.btn(@click="onDelete(todo)") Delete
 </template>
 
 <script>
@@ -45,11 +49,77 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    @import '../assets/scss/colors';
+
     .todo {
         margin-bottom: 20px;
 
-        &__checkbox {
-            margin: 0 7px;
+        // &__checkbox {
+        //     margin: 0 7px;
+        // }
+
+        &__control {
+            margin-top: 5px;
+        }
+
+        &__checkbox-container {
+            display: block;
+            position: relative;
+            padding-left: 35px;
+            margin-bottom: 12px;
+            cursor: pointer;
+            font-size: 22px;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+
+            &__checkbox {
+                position: absolute;
+                opacity: 0;
+                cursor: pointer;
+                height: 0;
+                width: 0;
+            }
+
+            &__checkmark {
+                position: absolute;
+                top: 0;
+                left: 0;
+                height: 31px;
+                width: 31px;
+                background-color: #eee;
+
+                &:after {
+                    content: "";
+                    position: absolute;
+                    display: none;
+                }
+            }
+
+            &:hover &__checkbox ~ &__checkmark {
+                background-color: $link-primary-color;
+            }
+
+            & &__checkbox ~ &__checkmark {
+                background-color: $header-title-color;
+            }
+
+            & &__checkbox:checked ~ &__checkmark:after {
+                display: block;
+            }
+
+            & &__checkmark:after {
+                left: 12px;
+                top: 7px;
+                width: 5px;
+                height: 10px;
+                border: solid white;
+                border-width: 0 3px 3px 0;
+                -webkit-transform: rotate(45deg);
+                -ms-transform: rotate(45deg);
+                transform: rotate(45deg);
+            }
         }
     }
 </style>
